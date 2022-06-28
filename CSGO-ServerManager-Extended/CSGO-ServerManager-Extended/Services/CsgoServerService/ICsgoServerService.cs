2e39ff6c3f0ed01570adca676157e17468450e8b
MartinSvanholm <msvanh20@student.aau.dk>
2022-06-28 16:23:19 +0200

    rework

--- CSGO-ServerManager-Extended/CSGO-ServerManager-Extended/Services/CsgoServerService/ICsgoServerService.cs
+++ CSGO-ServerManager-Extended/CSGO-ServerManager-Extended/Services/CsgoServerService/ICsgoServerService.cs
@@ -4,10 +4,17 @@ namespace CSGO_ServerManager_Extended.Services.CsgoServerService
 {
     public interface ICsgoServerService
     {
-        ICsgoServer SelectedServerforNavigation { get; set; }
+        ICsgoServer Server { get; set; }
 
+        Task<ICsgoServer> ChangeMap(string map);
         string GetCfg(string fileName);
         Task<DatHostServer> GetDatHostServer(string id);
         Task<List<DatHostServer>> GetDatHostServers();
+        Task<ICsgoServer> PauseUnpauseMatch(bool isMatchPaused);
+        Task<ICsgoServer> RunCommand(string command);
+        Task<ICsgoServer> StartKnife(string cfg = null);
+        Task<ICsgoServer> StartMatch(bool withOvertime, string cfg = null);
+        Task<ICsgoServer> StartNadePractice(string cfg = null);
+        Task<ICsgoServer> StartStopServer();
     }
 }
