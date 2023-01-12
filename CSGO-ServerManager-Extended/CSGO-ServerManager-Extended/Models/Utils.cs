85e8ecb7a9b1a52a0dca2bce9fd3b439fac4ef7b
Martin Svanholm <msvanh20@student.aau.dk>
2023-01-12 23:16:58 +0100

    Map pools is now premium

---
+++ CSGO-ServerManager-Extended/CSGO-ServerManager-Extended/Models/Utils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSGO_ServerManager_Extended.Models;
+
+public static class Utils
+{
+    public static bool IsPremiumAcount()
+    {
+        return false;
+    }
+}
